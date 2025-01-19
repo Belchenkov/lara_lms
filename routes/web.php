bd946058f62011b5295cff3df5ac90f5e676d7f1
Алексей Бельченков <u608110@gmail.com>
2025-01-19 12:47:33 +0400

    Auth admin - make user controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,15 +12,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::group([
+    'middleware' => ['auth:web', 'verified', 'check_role:student'],
+    'as' => 'user.',
+    'prefix' => 'user'
+], function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+});
+
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
 
