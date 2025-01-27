424eab7eb99061bf2dd788e4768635202f49cecb
Алексей Бельченков <u608110@gmail.com>
2025-01-27 12:12:42 +0400

    Auth admin - make content dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-       return view('admin.layouts.master');
+       return view('admin.dashboard');
     }
 }
